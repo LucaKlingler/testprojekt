5b2913e8a5003b4584b47dc75cf18a1dfa300f72
LucaKlingler <lucaklingler@outlook.de>
2020-06-04 10:15:18 +0200

    hilfe

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,35 +1,40 @@
 <template>
-  <div>
-    <table>
-      <th>Kategorie</th>
-          <th>Infos</th>
-          <th>Kosten</th>
-          <th>Tag</th>
-          <th>Downvotes</th>
-          <th>ID</th>
-          <th>Labels</th>
-          <th>Name</th>
-          <th>Upvotes</th>
-    </table> 
-    <meals v-for="mealsFromtTable in tableData" :key="mealsFromtTable" :meals="mealsFromtTable"></meals>
-  </div>
+<b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+    <b-carousel-slide class="slide" v-for="meals in mahlzeiten" :key="meals" img-src="https://picsum.photos/1024/480/?image=52"
+      ><meals class='mahlzeit' :data="meals"></meals></b-carousel-slide>
+</b-carousel>
 </template>
+
 <script>
 import meals from "../components/meals.vue"
   export default {
-    name: 'list',s
     props: {
-      tableData: String
+      mahlzeiten: Array
     }, 
     components: {
       meals
-    }
+    },
+    name: 'list'
   }
-  
 </script>
+
 <style scoped>
 .mahlzeit {
-  margin-bottom: 20px;
-  background-color:aquamarine;
+  margin-bottom: 50px;
+  max-width: 500px;
+  margin-left:auto;
+  margin-right:auto;
 }
 </style>
--- src/components/meals.vue
+++ src/components/meals.vue
@@ -1,27 +1,35 @@
 <template>
-<div>
-<table>
-  <tr>
-    <th>{{meals.category}}</th>
-    <td>{{meals.contentInformation}}</td>
-    <td>{{meals.cost}}</td>
-    <td>{{meals.day}}</td>
-    <td>{{meals.downvotes}}</td>
-    <td>{{meals.id}}</td>
-    <td>{{meals.labels}}</td>
-    <td>{{meals.name}}</td>
-    <td>{{meals.upvotes}}</td>
-  </tr>
-</table>
-</div>
+      <tr>
+          <th>{{meals.category}}</th>
+          <td>{{meals.contentInformation}}</td>
+          <td>{{meals.cost}}</td>
+          <td>{{meals.day}}</td>
+          <td>{{meals.downvotes}}</td>
+          <td>{{meals.id}}</td>
+          <td>{{meals.labels}}</td>
+          <td>{{meals.name}}</td>
+          <td>{{meals.upvotes}}</td>
+      </tr>
 </template>
 
 <script>
-  export default {
-    name: 'meals',
-    props: {
-      meals: Object
-    }
+export default {
+  name: 'meals',
+  props: {
+    meals: Object
   }
-
+}
 </script>
+
+<style scoped>
+* {
+  text-align: left;
+}
+
+
+.ci {
+  font-size: 8px;
+  display: inline-block;
+  margin-right:10px;
+}
+</style>
